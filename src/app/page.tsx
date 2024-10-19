ae44ca7b32b6811d018c44387ed35b18a9c7b321
amirhossein <salehpouramirhossein19@gmail.com>
2024-10-19 11:30:25 +0330

    Implementation of component card as static render Responsively

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,31 @@
-import Image from "next/image";
-import { Button } from "./_components/button";
+import { CourseSummary } from "@/types/course-summary.interface";
+import { HomeHeroSection } from "./_components/home-hero-section/HomeHeroSection";
+import { CourseCardList } from "./(courses)/courses/_components/course-card-list";
 
-export default function Home() {
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const res = await fetch(
+    `https://api.classbon.com/api/courses/newest/${count}`,
+    {
+      next: {
+        revalidate: 10,
+      },
+    }
+  );
+  return res.json();
+}
+
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4);
+
   return (
     <>
-      <section className="bg-hero-pattern bg-no-repeat bg-center mt-5 xl:mt-20 xl:bg-left">
-        <div className="container flex flex-col-reverse items-center xl:flex-row ">
-          <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right ">
-            <h3 className="text-xl dark:text-info xl:text-2xl">
-              خوش اومدی به ...
-            </h3>
-            <h1 className="text-3xl font-black gradient  lg:text-3xl xl:text-5xl">
-              مسیر صعود به قله های برنامه نویسی
-            </h1>
-            <p className="text-lg font-bold leading-8">
-              کلاسبن با ارائه آموزش های فرانت اند و با تمرکز ویژه بر آموزش React
-              در کنار شما توسعه دهندگان عزیز است تا فرآیند یادگیری را سریع تر،
-              پایدارتر و هدفمندتر طی کنید
-            </p>
-            <div className="my-5 flex gap-4">
-              <Button variant="primary" size="large">
-                دوره های نکست و ریکت
-              </Button>
-              <Button variant="neutral" size="large">
-                مشاوره برنامه نویسی
-              </Button>
-            </div>
-            <Image
-              className="grayscale mt-4 opacity-70 m-auto xl:m-0"
-              src="/images/frameworks.png"
-              width={412}
-              height={39}
-              alt=""
-            />
-          </div>
-          <Image
-            src="/images/programmer-landing.svg"
-            width={702}
-            height={521}
-            alt=""
-          />
+      <HomeHeroSection />
+      <section className="container pt-20">
+        <div className="text-center xl:text-right ">
+          <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
+          <p>برای به روز موندن، یادگرفتن نکته های تازه ضروریه</p>
         </div>
+        <CourseCardList courses={newestCourses} />
       </section>
     </>
   );
